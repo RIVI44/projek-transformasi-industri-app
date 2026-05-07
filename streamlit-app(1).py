99679e0a62fbf59e020fb822b33edfc88ad848f6
RIVI44 <riviamaharani0@gmail.com>
2026-05-07 13:52:18 +0700

    Update streamlit-app(1).py

--- streamlit-app(1).py
+++ streamlit-app(1).py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="POS Ayam Geprek",
+    page_title="Ayam Geprek AKA",
     page_icon="🍗",
     layout="wide",
     initial_sidebar_state="expanded"
